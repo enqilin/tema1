fcc1645f6342b7ccbfaa237e3eabad9101fb633d
enqilin <114181724+enqilin@users.noreply.github.com>
2023-03-02 22:53:24 +0000

    crear ayuda

---
+++ helpers.py
@@ -0,0 +1,15 @@
+
+import os
+import platform
+
+
+def limpiar_pantalla():
+    os.system('cls') if platform.system()=="Windows" else os.system('clear')
+
+def leer_texto(longitud_min=0,longitud_max=20,mensaje=None):
+    print(mensaje) if mensaje else  None
+    while True:
+        texto= inpur("> ")
+        if len(texto)>=longitud_min and len(texto)<=longitud_max:
+            return texto
+        

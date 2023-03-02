377ca38b0af19fbe7c41948751381979c4da11ec
enqilin <114181724+enqilin@users.noreply.github.com>
2023-03-02 22:53:34 +0000

    crear menu

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
-import os 
+import os
 import run
+import ejercicio
 
 def iniciar():
     while True:
@@ -12,23 +13,46 @@ def iniciar():
         print("[5] Ejercicio 5 ")
         print("[6] Ejercicio 6 ")
         print("[7] Ejercicio 7 ")
+        print("[8] Ejercicio 8 ")
         print("================")
 
         option= input ("> ")
         os.system("clear")
         if option==1:
+            print("Ejercicio 1 \n")
+            for numero1 in ejercicio/ejercicio1:
+                print(numero1)
         
         elif option==2:
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio2:
+                print(numero2)
 
         elif option==3:
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio3:
+                print(numero2)
         
         elif option==4:
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio4:
+                print(numero2)
 
         elif option==5:
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio5:
+                print(numero2)
 
         elif option==6:
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio6:
+                print(numero2)
         
         elif option==7:
-    
-    
-    break
+            print("Ejercicio  \n")
+            for numero2 in ejercicio/ejercicio7:
+                print(numero2)
+        elif option==8:
+            print("salir de gestor")
+            break
+        input("\nPresiona Enter para continuar...")

62b55c2beaf7b2b4a6c39601dc16182a2410c598
enqilin <114181724+enqilin@users.noreply.github.com>
2023-03-08 21:50:52 +0000

    el menu

--- menu.py
+++ menu.py
@@ -1,5 +1,12 @@
 import helpers
-import ejercicio as *
+from ejercicio import ejercicio1 as E1
+from ejercicio import ejercicio2 as E2
+from ejercicio import ejercicio3 as E4
+from ejercicio import ejercicio4 as E5
+from ejercicio import ejercicio6 as E6
+from ejercicio import ejercicio7 as E7
+from ejercicio import descomposion as dc
+
 
 def iniciar():
     while True:
@@ -16,42 +23,44 @@ def iniciar():
         print("================")
 
         option= input ("> ")
-        helpers.system("clear")
+
+        helpers.limpiar_pantalla()
+
         if option==1:
-            print("Ejercicio 1 \n")
-            for numero1 in ejercicio/ejercicio1:
+            print("Ejercicio 1\n")
+            for numero1 in :
                 print(numero1)
         
         elif option==2:
-            print("Ejercicio  \n")
+            print("Ejercicio 2\n")
             for numero2 in ejercicio/ejercicio2:
                 print(numero2)
 
         elif option==3:
-            print("Ejercicio  \n")
+            print("Ejercicio 3 \n")
             for numero2 in ejercicio/ejercicio3:
                 print(numero2)
         
         elif option==4:
-            print("Ejercicio  \n")
+            print("Ejercicio 4 \n")
             for numero2 in ejercicio/ejercicio4:
                 print(numero2)
 
         elif option==5:
-            print("Ejercicio  \n")
+            print("Ejercicio 5 \n")
             for numero2 in ejercicio/ejercicio5:
                 print(numero2)
 
         elif option==6:
-            print("Ejercicio  \n")
-            for numero2 in ejercicio/ejercicio6:
+            print("Ejercicio 6 \n")
+            for numero2 in ejercicio6.separar:
                 print(numero2)
-        
+
         elif option==7:
-            print("Ejercicio  \n")
+            print("Ejercicio 7 \n")
             for numero2 in ejercicio/ejercicio7:
                 print(numero2)
         elif option==8:
-            print("salir de gestor")
+            print("salir de ejercicio")
             break
         input("\nPresiona Enter para continuar...")
